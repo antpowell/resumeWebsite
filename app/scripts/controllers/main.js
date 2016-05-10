775ffd56083e8e76ac01d6de65f9f53cec2db5db
antpowell <powell.anthony.e@gmail.com>
2016-05-10 10:48:08 -0500

    Update Resume Page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name anthonyPowellcomApp.controller:MainCtrl
@@ -8,22 +7,56 @@
  * Controller of the anthonyPowellcomApp
  */
 angular.module('anthonyPowellcomApp')
-  .controller('MainCtrl', function() {
-    $(window).scroll(function(){
+  .controller('MainCtrl', function () {
+    var projectTop = $('.project').offset().top;
+    //var projectBottom = $('.project').offset().top + $('.project').height();
+    //var windowTop = $(window).scrollTop();
+    //var windowBottom = $(window).scrollTop() + $(window).height();
+
+
+    $(window).scroll(function () {
       var windowScroll = $(this).scrollTop();
+      var windowTop = $(this).scrollTop();
+      var windowBottom = $(this).scrollTop() + $(this).height();
+
+      var animationEnd = ('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
 
 
+      //console.log('Top: ' + projectTop + '  Bottom: ' + projectBottom);
+      console.log('Window Top: ' + windowTop + '  Window Bottom: ' + windowBottom);
+      if (windowBottom > projectTop) {
 
-      if(windowScroll>$('.jumbotron3').offset().top-$(window).height()){
-        console.log(windowScroll);
+        console.log('Window Scroll: ' + windowScroll);
+
+        //jQuery css manipulation version of project animation
+        //$('#BIT-mockup').css({
+        //  'transform': 'translate(-' + windowScroll / 35 + '%, 0px)'
+        //});
+        //$('#PF-mockup').css({
+        //  'transform': 'translate(' + windowScroll / 35 + '%, 0px)'
+        //});
+
+        //jQuery version of project animation
+        //$('#PF-mockup').animate({left: '750px'}, 800);
+        //$('#BIT-mockup').animate({right: '750px'}, 800, 'ease-out');
+
+
+        //Animation.css version of project animation
+        $('#PF-mockup').addClass('animated zoomInLeft').one(animationEnd,function(){
+          //$('#PF2-mockup').addClass('animated bounce');
+        });
+        $('#BIT-mockup').addClass('animated zoomInRight').one(animationEnd,function(){
+          //$(this).removeClass('animated zoomInRight');
+        });
+        setTimeout($('#PF2-mockup').addClass('animated bounce'),1000);
+
+      }else{
+        $('#PF-mockup').removeClass('animated zoomInLeft');
+        $('#BIT-mockup').removeClass('animated zoomInRight');
       }
 
-      $('#PF-mockup').css({
-        'transform':'translate('+windowScroll/50+'%, 0px)'
-      });
-      $('#BIT-mockup').css({
-        'transform':'translate(-'+windowScroll/50+'%, 0px)'
-      });
+
+
     });
 
   });
